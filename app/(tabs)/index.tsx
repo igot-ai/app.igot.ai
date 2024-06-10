ef9a047e349cb854af3a97e3650270aaa6c69a1c
Phan Duc Anh <anhphan190913@gmail.com>
2024-06-10 23:46:24 +0700

    feat: add structure homepage

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,64 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, View, Text, TouchableOpacity } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+import { useState } from 'react';
+import { cn } from '../../utils/cn';
+import ExploreScreen from '../home/explore';
+import YourBotsScreen from '../home/your-bots';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-
-export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+interface TabType {
+  id: number;
+  name: string;
+  component: React.ReactNode;
 }
 
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
+const tabs: TabType[] = [
+  { id: 1, name: 'Explore', component: <ExploreScreen /> },
+  { id: 2, name: 'Your bots', component: <YourBotsScreen /> },
+];
+
+export default function HomeScreen() {
+  const [tabActiveId, setTabActiveId] = useState<number>(1)
+
+  return (
+    <View className='h-full bg-white py-10 px-5'>
+      <View className="flex flex-row items-center w-100 mt-2">
+        <Image
+          source={require('../../assets/company-logo.png')}
+          style={{ width: 40, height: 40 }}
+        />
+        <View className='flex flex-1 flex-row justify-center items-center gap-10'>
+          {tabs.map((tab: TabType) => (
+            <TouchableOpacity
+              key={tab.id}
+              className={tab.id === tabActiveId ? 'border-b-2 border-b-gray-900' : ''}
+              onPress={() => setTabActiveId(tab.id)}
+            >
+              <Text
+                className={cn(
+                  ' py-2 text-sm font-medium',
+                  tab.id === tabActiveId ? 'text-gray-900' : 'text-gray-500'
+                )}
+              >
+                {tab.name}
+              </Text>
+            </TouchableOpacity>
+          ))}
+        </View>
+        <TouchableOpacity
+          // onPress={() => {
+          //   navigation.push('SearchScreen');
+          // }}
+          className='ml-auto'
+        >
+          <MaterialIcons
+            name="search"
+            size={24}
+            color="black"
+          />
+        </TouchableOpacity>
+      </View>
+      {tabs.find(tab => tab.id === tabActiveId)?.component}
+    </View>
+
+  );
+}
---
+++ app/home/explore.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function ExploreScreen() {
+  return (
+    <View>
+      <Text>Explore Screen</Text>
+    </View>
+  );
+}
